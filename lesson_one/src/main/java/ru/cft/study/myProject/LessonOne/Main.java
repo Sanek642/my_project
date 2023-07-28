aff39408c2662be3c6fc1960e08a81b835aff7c6
makogon <a.makogon@cft.ru>
2023-07-28 10:07:01 +0700

    Add multuTable

---
+++ lesson_one/src/main/java/ru/cft/study/myProject/LessonOne/Main.java
@@ -0,0 +1,7 @@
+package ru.cft.study.myProject.LessonOne;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ multiTable/src/main/java/ru/ctf/study/multiTable/Main.java
@@ -0,0 +1,54 @@
+package ru.ctf.study.multiTable;
+
+import java.util.Scanner;
+
+public class Main {
+    static int GetOrder(int n){
+        int count = 0;
+        while (n >= 10){
+            count += 1;
+            n = n/10;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println("Введите размер таблицы: ");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        System.out.printf("Размер таблицы: %d \n", num);
+        //Найдем предел циклов
+        int maxInt = num + 1;
+        // Найдем порядок числа
+        int maxTableRes = (maxInt) * (maxInt);
+        int n = GetOrder(maxTableRes) ;
+        String ots = ("_".repeat(n+1) + "+").repeat(num-1);
+        int p;
+        int result;
+        StringBuilder strJ = new StringBuilder();
+        for (int i = 1; i < maxInt; i++) {
+            for (int j = 1; j < maxInt; j++) {
+                result = i*j;
+                p = GetOrder(result);
+                if (j != num){
+                    strJ.append(" ".repeat(n - p)).append(result).append("|");
+                }
+                else {
+                    strJ.append(" ".repeat(n - p)).append(result);
+                }
+            }
+            if (i == 1) {
+                System.out.printf(" ".repeat(n) + "|" + strJ ) ;
+            }
+            else {
+                p = GetOrder(i);
+                System.out.printf(" ".repeat(n-p-1) + "%d" + "|" + strJ, i);
+            }
+
+            strJ = new StringBuilder();
+            System.out.printf("\n%s+%s%s\n", "_".repeat(n), ots, "_".repeat(n+1));
+        }
+
+
+    }
+}
