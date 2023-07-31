4adca67f7ead7be56795137846fd322e486f2299
makogon <a.makogon@cft.ru>
2023-07-31 11:09:06 +0700

    Add multuTable three virsion

--- multiTable/src/main/java/ru/ctf/study/multiTable/Main.java
+++ multiTable/src/main/java/ru/ctf/study/multiTable/Main.java
@@ -2,6 +2,8 @@ package ru.ctf.study.multiTable;
 
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class Main {
     static int GetOrder(int n){
         int count = 0;
@@ -13,41 +15,54 @@ public class Main {
     }
     public static void main(String[] args) {
         System.out.println("Введите размер таблицы: ");
-        Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
+        try {
+            Scanner in = new Scanner(System.in);
+            int num = abs(in.nextInt());
 
-        System.out.printf("Размер таблицы: %d \n", num);
-        //Найдем предел циклов
-        int maxInt = num + 1;
-        // Найдем порядок максимального числа
-        int maxTableRes = (maxInt) * (maxInt);
-        int n = GetOrder(maxTableRes) ;
-        // Строка отступа
-        String ots = ("_".repeat(n+1) + "+").repeat(num-1);
-        int p;
-        int result;
-        StringBuilder strJ = new StringBuilder();
-        for (int i = 1; i < maxInt; i++) {
-            for (int j = 1; j < maxInt; j++) {
-                result = i*j;
-                p = GetOrder(result);
-                if (j != num){
-                    strJ.append(" ".repeat(n - p)).append(result).append("|");
-                }
-                else {
-                    strJ.append(" ".repeat(n - p)).append(result);
-                }
-            }
-            if (i == 1) {
-                System.out.printf(" ".repeat(n) + "|" + strJ ) ;
+            System.out.printf("Размер таблицы: %d \n", num);
+            if (num == 1 || num == 0){
+                System.out.printf("%d", num);
             }
             else {
-                p = GetOrder(i);
-                System.out.printf(" ".repeat(n-p-1) + "%d" + "|" + strJ, i);
+                //Найдем предел циклов
+                int maxInt = num + 1;
+                // Найдем порядок максимального числа
+                int maxTableRes = (num) * (num);
+                int n;
+                if (num < 4){
+                    n = 1;
+                }
+                else {
+                    n = GetOrder(maxTableRes);
+                }
+                // Строка отступа
+                String ots = ("_".repeat(n + 1) + "+").repeat(num - 1);
+                int p;
+                int result;
+                StringBuilder strJ = new StringBuilder();
+                for (int i = 1; i < maxInt; i++) {
+                    for (int j = 1; j < maxInt; j++) {
+                        result = i * j;
+                        p = GetOrder(result);
+                        if (j != num) {
+                            strJ.append(" ".repeat(n - p)).append(result).append("|");
+                        } else {
+                            strJ.append(" ".repeat(n - p)).append(result);
+                        }
+                    }
+                    if (i == 1) {
+                        System.out.printf(" ".repeat(n) + "|" + strJ);
+                    } else {
+                        p = GetOrder(i);
+                        System.out.printf(" ".repeat(n - p - 1) + "%d" + "|" + strJ, i);
+                    }
+                    strJ = new StringBuilder();
+                    System.out.printf("\n%s+%s%s\n", "_".repeat(n), ots, "_".repeat(n + 1));
+                }
             }
-            strJ = new StringBuilder();
-            System.out.printf("\n%s+%s%s\n", "_".repeat(n), ots, "_".repeat(n+1));
         }
-
+         catch (Exception ex){
+            System.out.println("Ввелите целое число, меньше 2 147 483 647!");
+        }
     }
 }
