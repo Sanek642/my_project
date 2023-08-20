3516f28c38f24dc09b4327ca5e4a496c1a04c40f
makogon <a.makogon@cft.ru>
2023-08-20 15:08:34 +0700

    Add multitable

--- multiTable/src/main/java/ru/ctf/study/multiTable/Main.java
+++ multiTable/src/main/java/ru/ctf/study/multiTable/Main.java
@@ -1,11 +1,12 @@
 package ru.ctf.study.multiTable;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.Math.abs;
 
 public class Main {
-    static int GetOrder(int n){
+    static private int GetOrder(int n){
         int count = 0;
         while (n >= 10){
             count += 1;
@@ -13,42 +14,52 @@ public class Main {
         }
         return count;
     }
+
+    static private ArrayList<String> getMultiTable(int num){
+        ArrayList<String> multiTable = new ArrayList<String>();
+        if (num == 1 || num == 0) multiTable.add(Integer.toString(num));
+        else {
+            //  Найдем предел циклов
+            int maxInt = num + 1;
+            // Найдем порядок максимального числа
+            int maxTableRes = (num) * (num);
+            int n;
+            if (num < 9) n = 1;
+            else n = GetOrder(maxTableRes);
+            // Строка отступа
+            String ots = ("_".repeat(n + 1) + "+").repeat(num - 1);
+            int p;
+            int result;
+            StringBuilder strJ = new StringBuilder();
+            for (int i = 1; i < maxInt; i++) {
+                for (int j = 1; j < maxInt; j++) {
+                    result = i * j;
+                    p = GetOrder(result);
+                    if (j != num) strJ.append(" ".repeat(n - p)).append(result).append("|");
+                    else strJ.append(" ".repeat(n - p)).append(result);
+                }
+                if (i == 1) multiTable.add(" ".repeat(n) + "|" + strJ);
+                else {
+                    p = GetOrder(i);
+                    multiTable.add(" ".repeat(n - p - 1) + i + "|" + strJ);
+                }
+                strJ = new StringBuilder();
+                multiTable.add(String.format("\n%s+%s%s\n", "_".repeat(n), ots, "_".repeat(n + 1)));
+            }
+        }
+        return multiTable;
+    }
+
     public static void main(String[] args) {
         System.out.println("Введите размер таблицы: ");
         try {
             Scanner in = new Scanner(System.in);
             int num = abs(in.nextInt());
-
             System.out.printf("Размер таблицы: %d \n", num);
-            if (num == 1 || num == 0) System.out.println(num);
-            else {
-                //Найдем предел циклов
-                int maxInt = num + 1;
-                // Найдем порядок максимального числа
-                int maxTableRes = (num) * (num);
-                int n;
-                if (num < 9) n = 1;
-                else n = GetOrder(maxTableRes);
-                // Строка отступа
-                String ots = ("_".repeat(n + 1) + "+").repeat(num - 1);
-                int p;
-                int result;
-                StringBuilder strJ = new StringBuilder();
-                for (int i = 1; i < maxInt; i++) {
-                    for (int j = 1; j < maxInt; j++) {
-                        result = i * j;
-                        p = GetOrder(result);
-                        if (j != num) strJ.append(" ".repeat(n - p)).append(result).append("|");
-                        else strJ.append(" ".repeat(n - p)).append(result);
-                    }
-                    if (i == 1) System.out.print(" ".repeat(n) + "|" + strJ);
-                    else {
-                        p = GetOrder(i);
-                        System.out.print(" ".repeat(n - p - 1) + i + "|" + strJ);
-                    }
-                    strJ = new StringBuilder();
-                    System.out.printf("\n%s+%s%s\n", "_".repeat(n), ots, "_".repeat(n + 1));
-                }
+            ArrayList<String> multiTable = getMultiTable(num);
+            for (String stringMultiTable: multiTable
+                 ) {
+                System.out.print(stringMultiTable);
             }
         }
          catch (Exception ex){
